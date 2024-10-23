17ca53b5c9cb8f74a34a4aab468be5fb9753bde4
Axel Loeuillet <aloeuillet93@gmail.com>
2024-10-23 14:30:44 +0200

    start application

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use('/api/stuff', (req, res, next) => {
+
+    res.status(200).json({ message: 'coucou' });
+});
+
+module.exports = app;
